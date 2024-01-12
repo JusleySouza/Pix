b5fb00da42d46497449720c0fdf079a34894fae6
Jusley Souza <jusleysouza31@gmail.com>
2024-01-12 00:01:15 -0300

    Main Class

---
+++ src/main/java/br/com/pix/transaction/PixTransactionApplication.java
@@ -0,0 +1,13 @@
+package br.com.pix.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixTransactionApplication.class, args);
+	}
+
+}
