7bbef7c8928ce3b27303782eb3c20f0e047dba5e
Jusley Souza <jusleysouza31@gmail.com>
2024-01-16 22:33:36 -0300

    Creation and implementation of the BankAccountRepository class and the create method

---
+++ src/main/java/br/com/pix/transaction/repository/BankAccountRepository.java
@@ -0,0 +1,11 @@
+package br.com.pix.transaction.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.pix.transaction.model.BankAccount;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+
+}
