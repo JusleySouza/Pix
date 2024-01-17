ea26645243727bcba97531fb9519726b964810de
Jusley Souza <jusleysouza31@gmail.com>
2024-01-16 22:24:23 -0300

    Creation and implementation of the BankAccount model

---
+++ src/main/java/br/com/pix/transaction/model/BankAccount.java
@@ -0,0 +1,60 @@
+package br.com.pix.transaction.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import br.com.pix.transaction.constants.Conf;
+import br.com.pix.transaction.enums.AccountType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankAccount {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false, nullable = false)
+	private UUID id;
+	@Column(nullable = false)
+	private String fullName;
+	@Column(nullable = false)
+	private String agency;
+	@Column(nullable = false)
+	private String account;
+	@Column(nullable = false)
+	@Enumerated
+	private AccountType type;
+	@Column(nullable = false)
+	private String phone;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String document;
+	@Column(nullable = false)
+	private Double bankBalance;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private LocalDate created;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private LocalDate changed;
+	@Column(nullable = false)
+	private Boolean active;
+	
+}
