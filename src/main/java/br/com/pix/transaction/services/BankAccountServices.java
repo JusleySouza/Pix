9f6c787ae4c0d1559e443d6959e14d5a8b0358ee
Jusley Souza <jusleysouza31@gmail.com>
2024-01-19 20:29:32 -0300

    Creating the service to list all active accounts

--- src/main/java/br/com/pix/transaction/services/BankAccountServices.java
+++ src/main/java/br/com/pix/transaction/services/BankAccountServices.java
@@ -1,13 +1,18 @@
 package br.com.pix.transaction.services;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.pix.transaction.model.dto.RequestBankAccountDTO;
+import br.com.pix.transaction.model.dto.ResponseBankAccountDTO;
 
 @Service
 public interface BankAccountServices {
 	
 	public ResponseEntity<Object> create(RequestBankAccountDTO requestBankAccountDTO);
+	
+	public List<ResponseBankAccountDTO>findAll();
 
 }
