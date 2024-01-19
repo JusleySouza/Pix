fe07da5e99088b0a8673e6cb29f61ac3f84002fd
Jusley Souza <jusleysouza31@gmail.com>
2024-01-19 00:30:09 -0300

    Creating a search for existing and active documents

--- src/main/java/br/com/pix/transaction/repository/BankAccountRepository.java
+++ src/main/java/br/com/pix/transaction/repository/BankAccountRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.pix.transaction.model.BankAccount;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+	
+	BankAccount findByDocumentAndActiveTrue(String document);
 
 }
