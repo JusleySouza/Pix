63caed81bc9ad36e6d30951f707aed7fe3f82475
Jusley Souza <jusleysouza31@gmail.com>
2024-01-19 20:24:17 -0300

    Creating the route to list all accounts

--- src/main/java/br/com/pix/transaction/controller/BankAccountController.java
+++ src/main/java/br/com/pix/transaction/controller/BankAccountController.java
@@ -1,13 +1,18 @@
 package br.com.pix.transaction.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pix.transaction.model.dto.RequestBankAccountDTO;
+import br.com.pix.transaction.model.dto.ResponseBankAccountDTO;
 import br.com.pix.transaction.services.BankAccountServices;
 
 @RestController
@@ -21,4 +26,10 @@ public class BankAccountController {
 	public ResponseEntity<Object> create(@RequestBody RequestBankAccountDTO requestBankAccountDTO){
 		return services.create(requestBankAccountDTO);
 	}
+	
+	@GetMapping("/list")
+	public ResponseEntity<List<ResponseBankAccountDTO>> findAll(){
+		return new ResponseEntity<List<ResponseBankAccountDTO>>(services.findAll(), HttpStatus.OK);
+	}
+	
 }
