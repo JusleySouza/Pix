5bbc4117b052081e484d04e53da4124a60018522
Jusley Souza <jusleysouza31@gmail.com>
2024-01-19 22:16:47 -0300

    Creation of delete service

--- src/main/java/br/com/pix/transaction/services/BankAccountServices.java
+++ src/main/java/br/com/pix/transaction/services/BankAccountServices.java
@@ -1,10 +1,12 @@
 package br.com.pix.transaction.services;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.com.pix.transaction.model.BankAccount;
 import br.com.pix.transaction.model.dto.RequestBankAccountDTO;
 import br.com.pix.transaction.model.dto.ResponseBankAccountDTO;
 
@@ -14,5 +16,7 @@ public interface BankAccountServices {
 	public ResponseEntity<Object> create(RequestBankAccountDTO requestBankAccountDTO);
 	
 	public List<ResponseBankAccountDTO>findAll();
+	
+	public BankAccount delete(UUID accountId);
 
 }
