e304d73a8df46f51280479a11e9a0c902ed73105
Jusley Souza <jusleysouza31@gmail.com>
2024-01-19 22:44:54 -0300

    Implementation of reactivate service

--- src/main/java/br/com/pix/transaction/services/implement/BankAccountServicesImplement.java
+++ src/main/java/br/com/pix/transaction/services/implement/BankAccountServicesImplement.java
@@ -26,13 +26,13 @@ import jakarta.validation.Validator;
 
 @Component
 public class BankAccountServicesImplement implements BankAccountServices {
-	
+
 	@Autowired
 	private BankAccountRepository repository;
-	
+
 	@Autowired
 	private Validator validator;
-	
+
 	private BankAccount bankAccount;
 	private ResponseBankAccountDTO responseBankAccountDTO;
 	private List<BankAccount> listBankAccount;
@@ -41,30 +41,31 @@ public class BankAccountServicesImplement implements BankAccountServices {
 	@Override
 	public ResponseEntity<Object> create(RequestBankAccountDTO requestBankAccountDTO) {
 		Set<ConstraintViolation<RequestBankAccountDTO>> violations = validator.validate(requestBankAccountDTO);
-		
-		if(!violations.isEmpty()) {
+
+		if (!violations.isEmpty()) {
 			LoggerConfig.LOGGER_BANK_ACCOUNT.error("Validation error!");
-			return new ResponseEntity<Object>(ResponseError.createFromValidations(violations), HttpStatus.UNPROCESSABLE_ENTITY);
+			return new ResponseEntity<Object>(ResponseError.createFromValidations(violations),
+					HttpStatus.UNPROCESSABLE_ENTITY);
 		}
-		
+
 		bankAccount = BankAccountMapper.toModel(requestBankAccountDTO);
-		
+
 		duplicateAccountValidator(bankAccount);
-		
+
 		duplicatePixKeysValidator(bankAccount);
-		
+
 		repository.save(bankAccount);
-		
+
 		LoggerConfig.LOGGER_BANK_ACCOUNT.info("Bank account salved successfully!");
-		
+
 		return new ResponseEntity<Object>(HttpStatus.CREATED);
 	}
-	
+
 	@Override
 	public List<ResponseBankAccountDTO> findAll() {
 		listResponse = new ArrayList<>();
 		listBankAccount = repository.findAllByActiveTrue();
-		
+
 		for (BankAccount bankAccount : listBankAccount) {
 			responseBankAccountDTO = BankAccountMapper.modelToResponseBankAccountDTO(bankAccount);
 			listResponse.add(responseBankAccountDTO);
@@ -72,41 +73,57 @@ public class BankAccountServicesImplement implements BankAccountServices {
 		LoggerConfig.LOGGER_BANK_ACCOUNT.info("Bank account list successfully executed!");
 		return listResponse;
 	}
-	
+
 	@Override
 	public BankAccount delete(UUID accountId) {
-		bankAccount = repository.findById(accountId).orElseThrow(() ->
-			new ResourceNotFoundException("Sorry, we could not find a account with this id. Check and try again.")
-		);
-	
-		if(bankAccount.getActive() == Boolean.FALSE) {
+		bankAccount = repository.findById(accountId).orElseThrow(() -> new ResourceNotFoundException(
+				"Sorry, we could not find a account with this id. Check and try again."));
+
+		if (bankAccount.getActive() == Boolean.FALSE) {
 			throw new ValidationException("This account is already deactivated.");
 		}
-	
+
 		bankAccount = BankAccountMapper.deleteBankAccount(bankAccount);
 		repository.save(bankAccount);
-	
-		LoggerConfig.LOGGER_BANK_ACCOUNT.info("Bank account data " + bankAccount.getFullName() + " deleted successfully!");
+
+		LoggerConfig.LOGGER_BANK_ACCOUNT
+				.info("Bank account data " + bankAccount.getFullName() + " deleted successfully!");
 		return bankAccount;
 	}
+
+	@Override
+	public ResponseEntity<ResponseBankAccountDTO> reactivateAccount(UUID accountId) {
+		bankAccount = repository.findById(accountId).orElseThrow(() -> new ResourceNotFoundException(
+				"Sorry, we could not find a account with this id. Check and try again."));
+
+		if (bankAccount.getActive()) {
+			throw new ValidationException("This account is already active.");
+		}
+
+		bankAccount.setActive(Boolean.TRUE);
+		repository.save(bankAccount);
+
+		LoggerConfig.LOGGER_BANK_ACCOUNT.info("Bank account data " + bankAccount.getFullName() + " reactivated successfully!");
+		return new ResponseEntity<ResponseBankAccountDTO>(HttpStatus.NO_CONTENT);
+	}
+
 	
-	
-	
-	private void duplicateAccountValidator( BankAccount bankAccount) {
+	private void duplicateAccountValidator(BankAccount bankAccount) {
 		BankAccount accountEntity = repository.findByAccountAndActiveTrue(bankAccount.getAccount());
-		
-		if(accountEntity != null) {
+
+		if (accountEntity != null) {
 			throw new ValidationException("Unable to register bank account."
-					+ " There is already a customer registered with this account. Please check and try again.");	
+					+ " There is already a customer registered with this account. Please check and try again.");
 		}
 	}
-	
-	private void duplicatePixKeysValidator( BankAccount bankAccount) {
-		BankAccount keysEntity = repository.findByDocumentOrEmailOrPhoneAndActiveTrue(bankAccount.getDocument(), bankAccount.getEmail(), bankAccount.getPhone());
-		
-		if(keysEntity != null) {
+
+	private void duplicatePixKeysValidator(BankAccount bankAccount) {
+		BankAccount keysEntity = repository.findByDocumentOrEmailOrPhoneAndActiveTrue(bankAccount.getDocument(),
+				bankAccount.getEmail(), bankAccount.getPhone());
+
+		if (keysEntity != null) {
 			throw new DuplicateDocumentsException("Unable to register bank account."
-					+ " There is already a customer registered with this pix key. Please check the document, email or phone number and try again.");	
+					+ " There is already a customer registered with this pix key. Please check the document, email or phone number and try again.");
 		}
 	}
 
