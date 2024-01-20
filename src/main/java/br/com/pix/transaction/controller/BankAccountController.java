2f77055c12f1c8d550929eb7978733bb3ab8e5d0
Jusley Souza <jusleysouza31@gmail.com>
2024-01-19 22:16:06 -0300

    Creation and implementation of delete route

--- src/main/java/br/com/pix/transaction/controller/BankAccountController.java
+++ src/main/java/br/com/pix/transaction/controller/BankAccountController.java
@@ -1,11 +1,14 @@
 package br.com.pix.transaction.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +33,12 @@ public class BankAccountController {
 	@GetMapping("/list")
 	public ResponseEntity<List<ResponseBankAccountDTO>> findAll(){
 		return new ResponseEntity<List<ResponseBankAccountDTO>>(services.findAll(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{accountId}")
+	public ResponseEntity<Object> delete(@PathVariable("accountId") UUID accountId){
+		services.delete(accountId);
+		return new ResponseEntity<Object>( HttpStatus.NO_CONTENT);
 	}
 	
 }
